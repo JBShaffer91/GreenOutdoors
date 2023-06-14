09dc0b7102d6b65c0b7a6ce4cc8ad600c795909b
Justin Shaffer <justinbshaffer91@gmail.com>
2023-06-13 20:20:08 -0700

    Set up development environment

---
+++ GreenOutdoors.Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GreenOutdoors.Backend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
